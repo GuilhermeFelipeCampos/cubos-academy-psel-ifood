a2a5eed5025d899322e43da2633cf81955aa26de
Guilherme Felipe Campos <gui_fc@hotmail.com>
2021-06-08 21:50:33 -0300

    Esse Segundo foi Tranquilo!

---
+++ me_segue_no_insta.js
@@ -0,0 +1,12 @@
+function solucao(largura, altura) {
+    //seu codigo aqui
+      if(largura > altura){
+          console.log("PAISAGEM");
+      }else if(largura < altura){
+          console.log("RETRATO");
+       }else{
+           console.log("QUADRADA");
+       }
+    
+  }
+  
